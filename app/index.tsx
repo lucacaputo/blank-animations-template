e9ef15cd4e3c8e4723a19182620e8709652f12a9
Luca Caputo <luca.caputo@welld.ch>
2024-11-27 12:29:56 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+const Index = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Index;
